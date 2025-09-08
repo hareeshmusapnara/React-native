99a3cbc014ad0b4795ffea9eed98d2f4bf9720b7
hareeshmusapnara <harinhhdl143@gmail.com>
2025-09-08 22:28:00 +0530

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>
+        Home Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi
+        incidunt, laudantium quisquam perspiciatis nesciunt, architecto porro
+        numquam ut reprehenderit distinctio laboriosam accusamus placeat,
+        similique sint veritatis quas itaque enim. Eligendi doloremque dicta
+        deleniti? Dolor dolore sapiente reprehenderit hic. Illum, quod?
+      </Text>
+    </View>
+  );
+};
+
+export default Home;
+const styles = StyleSheet.create({});
