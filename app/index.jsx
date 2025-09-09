3b3a97f6a31d5c97e8fe7332b389d6d0047dd816
hareeshmusapnara <harinhhdl143@gmail.com>
2025-09-09 22:21:30 +0530

    second  commit

--- app/index.jsx
+++ app/index.jsx
@@ -1,18 +1,28 @@
-import { StyleSheet, Text, View } from "react-native";
-
+import { StyleSheet, Text, View, Image } from "react-native";
+import Logo from "../assets/img/logo_light.png";
 const Home = () => {
   return (
-    <View>
-      <Text>
-        Home Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi
-        incidunt, laudantium quisquam perspiciatis nesciunt, architecto porro
-        numquam ut reprehenderit distinctio laboriosam accusamus placeat,
-        similique sint veritatis quas itaque enim. Eligendi doloremque dicta
-        deleniti? Dolor dolore sapiente reprehenderit hic. Illum, quod?
-      </Text>
+    <View style={styles.container}>
+      <Image source={Logo} style={styles.img} />
+      <Text style={styles.title}> My Name Is Harish</Text>
+
+      <Text style={{ marginTop: 10, marginBottom: 30 }}> My Name Is Latha</Text>
     </View>
   );
 };
 
 export default Home;
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  img: {
+    marginVertical: 20,
+  },
+});
